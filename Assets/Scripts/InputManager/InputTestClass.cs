6b15c20172b001396a671092eb7f54cea3b450d0
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2023-11-25 01:37:45 +0900

    no message

---
+++ Assets/Scripts/InputManager/InputTestClass.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RSEngine;
+public class InputTestClass : MonoBehaviour
+{
+    InputPooler _iPooler;
+    private void Start()
+    {
+        _iPooler = GetComponent<InputPooler>();
+
+    }
+
+    private void Update()
+    {
+        Debug.Log(_iPooler.GetActionValueAs<bool>("Player", "Fire"));
+    }
+}
