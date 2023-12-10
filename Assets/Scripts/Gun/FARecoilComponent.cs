9e940fb22e47b5698ba1863d721142dead5e9e49
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2023-12-11 00:41:33 +0900

    [update]

--- Assets/Scripts/Gun/FARecoilComponent.cs
+++ Assets/Scripts/Gun/FARecoilComponent.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using RSEngine;
 using UnityEngine.InputSystem;
-/// <summary> 銃の反動を再現するコンポーネント </summary>
-public class RecoilComponent : MonoBehaviour
+/// <summary> 銃の反動を再現するコンポーネント (Full Auto) </summary>
+public class FARecoilComponent : MonoBehaviour
 {
     [SerializeField] Camera _cam;
     InputWindowLL _input;
     Vector2 _recoilDir;
+    float _elapsedTime;
 
     private void Start()
     {
@@ -26,9 +27,22 @@ public class RecoilComponent : MonoBehaviour
         Quaternion rotation = _cam.transform.rotation;
 
         if (_recoilDir != Vector2.zero)
-            rotation.x += _recoilDir.y * -Time.deltaTime;
-        else if(rotation.x < 0)
+        {
+            _elapsedTime += Time.deltaTime;
+
+            if (_elapsedTime > 1.0f / 30.0f)
+            {
+                Debug.Log("Bang!");
+                _elapsedTime = 0;
+
+                if (_recoilDir != Vector2.zero)
+                    rotation.x += _recoilDir.y * -Time.deltaTime;
+            }
+        }
+        else if (rotation.x < 0)
+        {
             rotation.x += Time.deltaTime;
+        }
 
         _cam.transform.rotation = rotation;
     }
